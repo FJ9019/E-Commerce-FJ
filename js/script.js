b845db5973f5eb7665d8b34b049f31f8c34f9440
FJ9019 <fenelus24@gmail.com>
2022-05-16 15:51:58 -0400

    Adding JavaScript

--- js/script.js
+++ js/script.js
@@ -1,18 +1,47 @@
-// init Isotope
-var $grid = $('.collection-list').isotope({
-    // options
-  });
-  // filter items on button click
-  $('.filter-button-group').on( 'click', 'button', function() {
-    var filterValue = $(this).attr('data-filter');
-    resetFilterBtns();
-    $(this).addClass('active-filter-btn');
-    $grid.isotope({ filter: filterValue });
-  });
+$(document).ready(function()
+{
 
-var filterBtns = $('.filter-button-group').find('button');
-function resetFilterBtns(){
-    filterBtns.each(function(){
-        $(this).removeClass('active-filter-btn');
+   
+        if($('.bbb_viewed_slider').length)
+        {
+            var viewedSlider = $('.bbb_viewed_slider');
+
+            viewedSlider.owlCarousel(
+            {
+                loop:true,
+                margin:30,
+                autoplay:true,
+                autoplayTimeout:6000,
+                nav:false,
+                dots:false,
+                responsive:
+                {
+                    0:{items:1},
+                    575:{items:2},
+                    768:{items:3},
+                    991:{items:4},
+                    1199:{items:6}
+                }
+            });
+
+            if($('.bbb_viewed_prev').length)
+            {
+                var prev = $('.bbb_viewed_prev');
+                prev.on('click', function()
+                {
+                    viewedSlider.trigger('prev.owl.carousel');
+                });
+            }
+
+            if($('.bbb_viewed_next').length)
+            {
+                var next = $('.bbb_viewed_next');
+                next.on('click', function()
+                {
+                    viewedSlider.trigger('next.owl.carousel');
+                });
+            }
+        }
+
+
     });
-}
